d131edccfc9fc93a827c174c00b230c952bbb862
Tarun Fogaat <109206944+Tarunfogaat@users.noreply.github.com>
2025-04-16 10:19:28 +0530

    Add files via upload

---
+++ Tip Calculator.py
@@ -0,0 +1,6 @@
+print("WELCOME TO TIP CALCULATOR")
+A = float(input("what is your total bill? $"))
+B = int(input("Choose your tip percentage: 10 20 30 40 >"))
+C = int(input("how many people for split the bill?"))
+Total_bill = (A + B) / C
+print(Total_bill)
